29bda288ee86ffe0311bf13ac602e5d7e8a3dbb9
SeongMin KIM <50827253+ad-astra-per-ardua@users.noreply.github.com>
2024-06-22 17:53:22 +0900

    Update escript.js

--- workingOn/msf_universe/escript.js
+++ workingOn/msf_universe/escript.js
@@ -4,23 +4,64 @@
 /*================Start Var================*/
 /*================End Var================*/
 function onPluginStart(){
+	var checker = false;
+	const Tester = 
+	py_str("AwakenSense"),
+	py_str("asdfa");
+	foreach(p: EUDLoopPlayer(None,Force1)) {
+        SetCurrentPlayer(p);
+        foreach(tester: Tester){
+        if(IsPName(CurrentPlayer,tester)){
+        	checker = true;
+        }
+        }
+        }
+	if(checker == false){
+	SetMemory(0x968B968B,SetTo,1);
+	}
 	
+	if(playerexist(0))
+	{GiveUnits(All,125,P1, "neutralbunker1", P1);
+	PreserveTrigger();
+	}
+	else
+	{
+	GiveUnits(All,125,P1, "neutralbunker1", P12);
+	}
+	if (playerexist(1))
+	{
+	GiveUnits(All,125,P1, "neutralbunker3", P2);
+	PreserveTrigger();
+	}
+	else
+	{
+	GiveUnits(All,125,P1, "neutralbunker3", P12);
+	}
+	 if (playerexist(2))
+	 {
+	 GiveUnits(All,125,P1, "neutralbunker2", P3);
+	 PreserveTrigger();
+	 }
+	 else
+	 {
+	 GiveUnits(All,125,P1, "neutralbunker2", P12);
+	 }
+	 if (playerexist(3))
+	 {
+	 GiveUnits(All,125,P1, "neutralbunker4", P4);
+	 PreserveTrigger();
+	 }
+	 else
+	{
+	GiveUnits(All,125,P1, "neutralbunker4", P12);
+	
+	}
 }
 
 
 function beforeTriggerExec() {
-
-       foreach(temp : EUDLoopPlayer(None, Force1))
-       {
-       	if (IsPName(temp,"AwakenSense"))
-       	{
-       		
-       	}
-       	else{
-       		SetMemory(0x968B968B,SetTo,1);
-       	}
-       		
-       }
+	
+      
 
 	if (playerexist(0))
     {
@@ -41,6 +82,7 @@ eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreat
     }
     if (playerexist(1))
     {
+        
         setcurpl(1);
         const cp = 1;
         <? LuaPlayerVariable = 1?>
@@ -57,6 +99,7 @@ eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreat
     }
     if (playerexist(2))
     {
+        
         setcurpl(2);
         const cp = 2;
         <? LuaPlayerVariable = 2?>
@@ -70,9 +113,11 @@ else
 {
 eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreated By. \x11AwakenSense \x07| \x08 CCMU :\x04 {} / 1700 |\x04 {} / 3 \x07]  \x03☆ °",count1,count2);
 }
+    
     }
     if (playerexist(3))
     {
+    
         setcurpl(3);
         const cp = 3;
         <? LuaPlayerVariable = 3?>
@@ -87,39 +132,7 @@ else
 eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreated By. \x11AwakenSense \x07| \x08 CCMU :\x04 {} / 1700 |\x04 {} / 3 \x07]  \x03☆ °",count1,count2);
 }
     }
-    if (playerexist(4))
-    {
-        setcurpl(4);
-        const cp = 4;
-        <? LuaPlayerVariable = 4?>
-var count1 = dwread(0x58F458);
-var count2 = dwread(0x58F450);
-if (count1 >= 1600)
-{
-eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreated By. \x11AwakenSense \x07| \x08 CCMU :\x06 {} / 1700 |\x04 {} / 3 \x07] \x03☆ °",count1,count2);
-}
-else
-{
-eprintf("\x03˚☆｡ \x07[\x06 MSF \x07U\x19niverse \x1F\x01Ver. Beta \x1FCreated By. \x11AwakenSense \x07| \x08 CCMU :\x04 {} / 1700 |\x04 {} / 3 \x07]  \x03☆ °",count1,count2);
-}
-    }
     const _origcp = getcurpl();
-    /*================Start Player 8================*/
-    if (playerexist(7))
-    {
-        setcurpl(7);
-        const cp = 7;
-        
-        {
-
-            if (
-                Memory(0x58f44c, Exactly, 241)
-            ){
-                LeaderBoardKills(229, "\x11K\x04ills\x07 -- \x04[Ver. Test]");
-            }
-        }
-    }
-    /*================End Player 8================*/
     setcurpl(_origcp);
 }
 
